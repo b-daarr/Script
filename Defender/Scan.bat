4b1bc93c4db668e9c05e35f8128bb1aec22acfc9
b̴d̴aar <altoni00161514@gmail.com>
2024-04-11 02:17:56 +0330

    repair file

--- Defender/Scan.bat
+++ Defender/Scan.bat
@@ -6,31 +6,21 @@ TITLE Note!
 SET from=5
 SET oper=-1
 SET end=0
-FOR /L %%A IN (%from%,%oper%,%end%) DO (@ECHO. && ECHO --==--==--==--==--==--==--==--==--==--==--==--==--==--==-- && ECHO --==--==--== This Scan May Take a Long Time ==--==--==-- && ECHO --==--==--==--==--==--==--==--==--==--==--==--==--==--==-- && TIMEOUT /T 1 > Cache && CLS)
+FOR /L %%A IN (%from%,%oper%,%end%) DO (@ECHO. && ECHO --==--==--==--==--==--==--==--==--==--==--==--==--==--==-- && ECHO --==--==--== This Scan May Take a Long Time ==--==--==-- && ECHO --==--==--==--==--==--==--==--==--==--==--==--==--==--==-- && TIMEOUT /T 1 > Cache.null && CLS)
 
 DEL Cache.null
 
 COLOR 0C
-MODE COn COLS=100 LINES=15
+MODE CON COLS=100 LINES=15
 TITLE Defending!
 ECHO ----------------------------------------------------------
 ECHO.
 ECHO    DDDDDDD    EEEEEEEEE  FFFFFFFFF  EEEEEEEEE  NNN    NN  DDDDDDD    II  NNN    NN  GGGGGGGGG
 ECHO    DD   DDD   EE         FF         EE         NN N   NN  DD   DDD   II  NN N   NN  GG
 ECHO    DD    DDD  EEEEEEE    FFFFFF     EEEEEEE    NN  N  NN  DD    DDD  II  NN  N  NN  GG   GGGG
-ECHO    DD   DDD   EE         FF         EE         NN   N NN  DD   DDD   II  NN   N NN  GG     GG
-ECHO    DDDDDDD    EEEEEEEEE  FF         EEEEEEEEE  NN    NNN  DDDDDDD    II  NN    NNN  GGGGGGGGG
+ECHO    DD   DDD   EE         FF         EE         NN   N NN  DD   DDD   II  NN   N NN  GG     GG  ..  ..  ..
+ECHO    DDDDDDD    EEEEEEEEE  FF         EEEEEEEEE  NN    NNN  DDDDDDD    II  NN    NNN  GGGGGGGGG  ..  ..  ..
 ECHO.
 ECHO ----------------------------------------------------------
-"%ProgramFiles%\Windows Defender\MpCmdRun.exe" -Scan -File C:\ -ScanType 1
-CLS
-ECHO ----------------------------------------------------------
-ECHO.
-ECHO    CCCCCCCCC    OOOOOOOOO    MMMMMMM     MMMMMMM  PPPPPP
-ECHO    CC         OO         OO  MMMMMMMM   MMMMMMMM  PP   PP
-ECHO    CC         OO         OO  MMM   MMM MMM   MMM  PPPPPP
-ECHO    CC         OO         OO  MMM    MMMM     MMM  PP
-ECHO    CCCCCCCCC    OOOOOOOOO    MMM             MMM  PP
-ECHO.
-ECHO ----------------------------------------------------------
-PAUSE
+"%ProgramFiles%\Windows Defender\MpCmdRun.exe" -Scan -ScanType 1
+EXIT
