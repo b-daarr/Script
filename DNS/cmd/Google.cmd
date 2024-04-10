c6bcb97db256200146fb467d802db23bd6594125
b̴d̴aar <altoni00161514@gmail.com>
2024-04-10 08:34:22 +0330

    ..

--- DNS/cmd/Google.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-netsh interface ipv4 set dnsservers "Wi-Fi" static 8.8.8.8 primary
-netsh interface ipv4 add dnsservers "Wi-Fi" 8.8.4.4 index=2
-netsh interface ipv4 set dnsservers "Ethernet" static 8.8.8.8 primary
-netsh interface ipv4 add dnsservers "Ethernet" 8.8.4.4 index=2
-exit
--- DNS/cmd/Null.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-netsh interface ipv4 delete dnsservers "Ethernet" all
-netsh interface ipv4 delete dnsservers "Wi-Fi" all
-exit
