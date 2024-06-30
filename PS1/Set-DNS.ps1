c970b2733079cc9d767b04931dd001148dfef4ca
b̴⊶d̴aarr <118155606+b-daarr@users.noreply.github.com>
2024-07-01 01:54:17 +0330

    Update Set-DNS.ps1

--- PS1/Set-DNS.ps1
+++ PS1/Set-DNS.ps1
@@ -1,3 +1,4 @@
+Get-NetIPInterface | Sort-Object -Property InterfaceIndex | Format-Table
 for (($i = 1); $i -lt 50; $i++)
 {
     Set-DnsClientServerAddress -InterfaceIndex $i -ServerAddresses("8.8.8.8", "8.8.4.4")
